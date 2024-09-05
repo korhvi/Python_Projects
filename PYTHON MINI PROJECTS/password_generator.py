f88d8d94dfbfafa4dab9115c599237e78a8b704d
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-09-05 15:00:07 +0300

    made to crypt pwd and decrypt

--- PYTHON MINI PROJECTS/password_generator.py
+++ PYTHON MINI PROJECTS/password_generator.py
@@ -1,5 +1,6 @@
 import random
 import string
+from cryptography.fernet import Fernet
 
 def generate_password(min_length, numbers = True, special_character = True):
   letters = string.ascii_letters
@@ -34,17 +35,49 @@ def generate_password(min_length, numbers = True, special_character = True):
 
   return pwd
 
-def add():
-  with open ("password.txt", "a") as f:
-    f.write(pwd + "\n")
+def add(enc_pwd):
+    with open("password.txt", "a") as f:
+        f.write(enc_pwd.decode() + "\n")
 
-  
+def view():
+    with open("password.txt", "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                try:
+                    decrypted_passw = fernet.decrypt(line.encode()).decode()
+                    print("Decrypted password:", decrypted_passw)
+                except Exception as e:
+                    print(f"Failed to decrypt password: {e}")
+
+def save_key(key):
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    with open("key.key", "rb") as key_file:
+        return key_file.read()
+
+try:
+    key = load_key()
+    fernet = Fernet(key)
+except FileNotFoundError:
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    save_key(key)
+
 min_length = int(input("Enter the minimum length: "))
-has_number = input("Do you want to have a numbers (y/n)? ").lower() == "y"
-has_special = input("Do you want to special characters (y/n)? ").lower() == "y"
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_special = input("Do you want special characters (y/n)? ").lower() == "y"
 pwd = generate_password(min_length, has_number, has_special)
-answer = input("Do you want to save password (y/n)? ")
+
+enc_pwd = fernet.encrypt(pwd.encode())
+answer = input("Do you want to save the password (y/n)? ")
 if answer == "y":
-  add()
+    add(enc_pwd)
+
+load = input("Do you want to see saved passwords (y/n)? ")
+if load == "y":
+    view()
 else:
-  print("The generated password is:", pwd)
+    print("The generated password is:", pwd)
