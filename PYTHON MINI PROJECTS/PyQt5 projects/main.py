29036f37e472db506a8246001b2cd0659655e293
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-09-08 18:30:03 +0300

    added layouts

--- PYTHON MINI PROJECTS/PyQt5 projects/main.py
+++ PYTHON MINI PROJECTS/PyQt5 projects/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout,          QGridLayout)
 from PyQt5.QtGui import QFont, QIcon, QPixmap
 from PyQt5.QtCore import Qt
 
@@ -9,9 +9,9 @@ class MainWindow(QMainWindow):
     self.setWindowTitle("MY cool first HUI")
     self.setGeometry(700, 300, 500, 500)
     self.setWindowIcon(QIcon("PYTHON MINI PROJECTS/PyQt5 projects/dog.jpg"))
-
-    label = QLabel( self)
+    self.initUI()
     '''
+    label = QLabel( self)
     label.setFont(QFont("Arial", 40))
     label.setGeometry(0,0, 500, 100)
     label.setStyleSheet("color: blue;"
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
                         "font-style: italic;"
                         "text-decoration: underline;")
     
-    label.setAlignment(Qt.AlignCenter) '''
+    label.setAlignment(Qt.AlignCenter)
     
     pixmap = QPixmap("PYTHON MINI PROJECTS/PyQt5 projects/dog.jpg")
     label.setGeometry(0,0, 250, 250)
@@ -30,8 +30,35 @@ class MainWindow(QMainWindow):
     label.setGeometry((self.width() - label.width()) // 2, 
                       (self.height() - label.height()) // 2, 
                       label.width(), 
-                      label.height())
-    
+                      label.height())'''
+
+  def initUI(self):
+    central_widget = QWidget()
+    self.setCentralWidget(central_widget)
+
+    label1 = QLabel("#1", self)
+    label2 = QLabel("#2", self)
+    label3 = QLabel("#3", self)
+    label4 = QLabel("#4", self)
+    label5 = QLabel("#5", self)
+
+    label1.setStyleSheet("background-color: red;")
+    label2.setStyleSheet("background-color: yellow;")
+    label3.setStyleSheet("background-color: green;")
+    label4.setStyleSheet("background-color: blue;")
+    label5.setStyleSheet("background-color: purple;")
+
+    vbox = QVBoxLayout()
+
+    vbox.addWidget(label1)
+    vbox.addWidget(label2)
+    vbox.addWidget(label3)
+    vbox.addWidget(label4)
+    vbox.addWidget(label5)
+
+    central_widget.setLayout(vbox)
+
+
 
 
 def main():
