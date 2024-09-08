c746279422ddb2d7bee89103cfe028f408fbd1b9
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-09-08 18:09:58 +0300

    added labels

--- PYTHON MINI PROJECTS/PyQt5 projects/main.py
+++ PYTHON MINI PROJECTS/PyQt5 projects/main.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -8,6 +10,17 @@ class MainWindow(QMainWindow):
     self.setWindowTitle("MY cool first HUI")
     self.setGeometry(700, 300, 500, 500)
     self.setWindowIcon(QIcon("dog.jpg"))
+
+    label = QLabel("Hello", self)
+    label.setFont(QFont("Arial", 40))
+    label.setGeometry(0,0, 500, 100)
+    label.setStyleSheet("color: blue;"
+                        "background-color: gray;"
+                        "font-weight: bold;"
+                        "font-style: italic;"
+                        "text-decoration: underline;")
+    
+    label.setAlignment(Qt.AlignCenter)
 
 
 def main():
